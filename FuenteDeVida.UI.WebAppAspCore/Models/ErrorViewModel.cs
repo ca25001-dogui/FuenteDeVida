3368f2b87de2223f6c59f8d928bd1d4074d781fd
Veronica Moran <veronicamoran25@outlook.com>
2026-03-04 20:35:22 -0600

    Creación de capa UI

---
+++ FuenteDeVida.UI.WebAppAspCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FuenteDeVida.UI.WebAppAspCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
