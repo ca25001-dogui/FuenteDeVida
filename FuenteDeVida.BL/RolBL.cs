eb9c062e26c76adea173beb8fb4d59e29274f2bd
Kevin Canales <kevscanales@outlook.com>
2026-03-05 21:32:27 -0600

    trabajo rol y usuario BL

--- FuenteDeVida.BL/RolBL.cs
+++ FuenteDeVida.BL/RolBL.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using FuenteDeVida.DAL;
+using FuenteDeVida.EN;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FuenteDeVida.BL
 {
-    internal class RolBL
+    public class RolBL
     {
+        public int Guardar(RolBL pRol)
+        {
+            return RolDAL.Rol(pRol);
+        }
+
+        public int Modificar(RolBL pRol)
+        {
+            return RolDAL.Modificar(pRol);
+        }
+
+        public int Eliminar(RolBL pRol)
+        {
+            return RolDAL.Eliminar(pRol);
+        }
+
+        public Rol ObtenerPorId(short pIdRol)
+        {
+            return RolDAL.ObtenerPorId(pIdRol);
+        }
+
+        public List<Rol> Buscar(RolBL pRol)
+        {
+            return RolDAL.Buscar(pRol);
+        }
     }
+
 }
--- FuenteDeVida.BL/UsuarioBL.cs
+++ FuenteDeVida.BL/UsuarioBL.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿namespace FuenteDeVida.BL
+//Referencias
+using System.Security.Cryptography;
+// Referencias del proyecto
+using FuenteDeVida.DAL;
+using FuenteDeVida.EN;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace FuenteDeVida.BL
 {
-    internal class UsuarioBL
+    public class RolBL
     {
+        private static string CifrarSha256(string pTexto)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(pTexto); // Codificar el texto a bytes
+                                                              // Crear y usar SHA256 de forma segura
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hash = sha256.ComputeHash(bytes);
+                // Convertir hash a cadena hexadecimal
+                StringBuilder hashString = new StringBuilder();
+                foreach (byte x in hash)
+                {
+                    hashString.AppendFormat("{0:x2}", x);
+                }
+                return hashString.ToString();
+            }
+        }
+
+        public int Guardar(Usuario pUsuario)
+        
     }
 }
