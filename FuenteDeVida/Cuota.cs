38821e078ef8e0b7d11accf1c35fbcd45472adee
Nayeli Campos <nayelicampos366@gmail.com>
2026-03-10 06:26:11 -0600

    Codificación de la capa EN de las clases Factura y Cuota

--- FuenteDeVida/Cuota.cs
+++ FuenteDeVida/Cuota.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace FuenteDeVida.EN
 {
-    internal class Cuota
+    public class Cuota
     {
+        public string Descripcion;
+        /// <summary>
+        /// <b>PK:</b> Llave primaria de cuota
+        /// </summary>
+        public int IdCuota { get; set; }
+
+        public int IdComunidad { get; set; }
+
+        public decimal Monto { get; set; }
+        public string TipoServicio { get; set; } = string.Empty;
+        public DateTime FechaLimite { get; set; }
+
+        /// <summary>
+        /// <b>FK:</b> Llave foranea de cuota que tiene relación con la entidad comunidad
+        /// </summary>
+        public short Comunidad { get; set; }
+
+        /// <summary>
+        ///  Propiedad virtual de <b>IdCargo (FK)</b> para representar la Asociacion
+        /// </summary> 
+        public virtual Comunidad Comunidad { get; set; } = new Comunidad();
+        
     }
 }
+ 
--- FuenteDeVida/Factura.cs
+++ FuenteDeVida/Factura.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace FuenteDeVida.EN
 {
-    internal class Factura
+    public class Factura
     {
+        public string Descripcion;
+        /// <summary>
+        /// <b>PK:</b> Llave primaria de Factura
+        /// </summary>
+        public int IdFactura { get; set; }
+
+        public int IdUsuario { get; set; }
+        public int IdComunidad { get; set; }
+
+        public DateTime FechaEmision { get; set; }
+        public decimal Total { get; set; }
+
+
+        /// <summary>
+        /// <b>FK:</b> Llave foranea de factura que tiene relación con la entidad usuario
+        /// </summary>
+        public short Usuario { get; set; }
+
+        /// <summary>
+        ///  Propiedad virtual de <b>IdCargo (FK)</b> para representar la Asociacion
+        /// </summary> 
+        public virtual Usuario Usuario { get; set; } = new Usuario();
+
+        /// <summary>
+        /// <b>FK:</b> Llave foranea de factura que tiene relación con la entidad comunidad
+        /// </summary>
+        public short Comunidad { get; set; }
+
+        /// <summary>
+        ///  Propiedad virtual de <b>IdCargo (FK)</b> para representar la Asociacion
+        /// </summary> 
+        public virtual Comunidad Comunidad { get; set; } = new Comunidad();
+
     }
+
 }
+
