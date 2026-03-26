4ce79d6cacfd3e924641989d51fda92c7155aa5c
Veronica Moran <veronicamoran25@outlook.com>
2026-03-25 18:12:05 -0600

    Codificación de vista de Multa y agg detalles en el Layout e index para Home

--- FuenteDeVida/Usuario.cs
+++ FuenteDeVida/Usuario.cs
@@ -8,20 +8,38 @@ using System.Threading.Tasks;
 
 namespace FuenteDeVida.EN
 {
-   public class Usuario
+    public class Usuario
     {
         [Key]
         public int IdUsuario { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public string Apellido {  get; set; } = string.Empty;
-        public string Correo {  get; set; } = string.Empty;
-        public string Clave {  get; set; } = string.Empty;
-
-        [NotMapped]
-        public int Top_Aux { get; set; }
 
         //Llave foranea hacia Administrador 
-        public int IdRol {  get; set; }
-        public virtual Rol Rol { get; set; } = new Rol();
+        [ForeignKey("Rol")]
+        [Required(ErrorMessage = "Rol es obligatorio")]
+        [Display(Name = "Rol")]
+        public int IdRol { get; set; }
+
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Apellido es obligatorio")]
+        [StringLength(30, ErrorMessage = "Maximo 30 caracteres")]
+        public string Apellido { get; set; }
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [StringLength(25, ErrorMessage = "Maximo 25 caracteres")]
+        public string Correo { get; set; }
+        [Required(ErrorMessage = "La clave es obligatorio")]
+        [DataType(DataType.Password)]
+        [StringLength(32, ErrorMessage = "La Clave debe estar entre 5 a 32 caracteres", MinimumLength = 5)]
+        public string Clave { get; set; }
+        public Rol Rol { get; set; }
+        [NotMapped]
+        public int Top_Aux { get; set; }
+    }
+    public enum Estatus_Usuario
+    {
+        ACTIVO = 1,
+        INACTIVO = 2
     }
 }
+
